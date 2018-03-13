459d44a1cd84812f428377ef50b3a8e2a8fab8f2
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 16:25:02 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     return render :edit unless @post.update(post_params)
-
     redirect_to post_path(@post)
   end
 
